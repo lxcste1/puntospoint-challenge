a0dbe23ba950ea4c0d98d58f1b5ff3bc20832d8c
lxcste <lucas.tello@pinapp.tech>
2024-01-19 12:51:52 -0300

    finish the tables for pulse view

--- src/components/MainContent/components/Pulse/PulseChart/PulseChart.tsx
+++ src/components/MainContent/components/Pulse/PulseChart/PulseChart.tsx
@@ -9,6 +9,7 @@ import { Container, Grid, ToggleButtonGroup, ToggleButton } from '@mui/material'
 
 import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Legend, PointElement, LineElement, Tooltip } from 'chart.js/auto';
 import { Chart } from 'react-chartjs-2';
+import Tables from './components/Tables';
 
 ChartJS.register(
     CategoryScale,
@@ -70,43 +71,52 @@ export default function PulseChart() {
 
     return (
         <>
-            <Grid container>
-                <Grid item md={12} sx={{marginY:"0.75rem"}}>
-                    <ToggleButtonGroup
-                        color="primary"
-                        value={view}
-                        exclusive
-                        onChange={handleChangeView}
-                        aria-label="View"
-                        sx={{flexWrap:["wrap","nowrap"], justifyContent:["center", "unset"]}}>
-                        {[{name:"Todos"}, {name:"Sep"}, {name:"Oct"}, {name:"Nov"}].map((e) => (
-                            <ToggleButton 
-                                key={e.name} 
-                                value={e.name} 
-                                sx={{
-                                    marginX:["2px", "0.75rem"],
-                                    padding:["3px 8px", "6px 12px"],
-                                    height:["min-content", "100%"],
-                                    fontSize:["12px","14px"],
-                                    border:"none",
-                                    textTransform:"inherit",
-                                    borderRadius:"8px!important",
-                                    color:"#1D192B",
-                                    "&.Mui-selected":{
+            <Container sx={{padding:"0"}}>
+                <Grid container>
+                    <Grid item md={12} sx={{marginY:"0.75rem"}}>
+                        <ToggleButtonGroup
+                            color="primary"
+                            value={view}
+                            exclusive
+                            onChange={handleChangeView}
+                            aria-label="View"
+                            sx={{flexWrap:["wrap","nowrap"], justifyContent:["center", "unset"]}}>
+                            {[{name:"Todos"}, {name:"Sep"}, {name:"Oct"}, {name:"Nov"}].map((e) => (
+                                <ToggleButton 
+                                    key={e.name} 
+                                    value={e.name} 
+                                    sx={{
+                                        marginX:["2px", "0.75rem"],
+                                        padding:["3px 8px", "6px 12px"],
+                                        height:["min-content", "100%"],
+                                        fontSize:["12px","14px"],
+                                        border:"none",
+                                        textTransform:"inherit",
+                                        borderRadius:"8px!important",
                                         color:"#1D192B",
-                                        backgroundColor:"#E7DFF8"
-                                        }
-                                    }}>
-                                {e.name}
-                            </ToggleButton>
-                        ))}
-                    </ToggleButtonGroup>                        
-                </Grid>    
-            </Grid>
+                                        "&.Mui-selected":{
+                                            color:"#1D192B",
+                                            backgroundColor:"#E7DFF8"
+                                            }
+                                        }}>
+                                    {e.name}
+                                </ToggleButton>
+                            ))}
+                        </ToggleButtonGroup>                        
+                    </Grid>    
+                </Grid>
+            </Container>
             <Container sx={{padding:"0", marginTop:"1rem"}}>
                 <Grid container sx={{justifyContent:"center"}}>
                     <Grid item md={12} xs={11} sx={{maxWidth:"100%", flexBasis:"100%"}}>
                         {view == 'Todos' && <Chart type='bar' data={dataChartPulse} options={options} height={400} />}
+                    </Grid>
+                </Grid>
+            </Container>
+            <Container sx={{padding:"0"}}>
+                <Grid container sx={{justifyContent:"center"}}>
+                    <Grid item md={12} xs={11} sx={{maxWidth:"100%", flexBasis:"100%"}}>
+                        <Tables data={data} />
                     </Grid>
                 </Grid>
             </Container>
